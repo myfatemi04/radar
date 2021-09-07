01d0b0d66baefadfb0ce983a21f9f06e89f80cab
Michael Fatemi <myfatemi04@gmail.com>
2021-09-06 22:16:09 -0400

    show name of current item in document title

--- src/RootItemRenderer.tsx
+++ src/RootItemRenderer.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
 import RootItem from './RootItem';
 import useItem from './useItem';
 
 export default function RootItemRenderer({ id }: { id: string }) {
 	const item = useItem(id);
+
+	useEffect(() => {
+		if (item?.name) {
+			document.title = `${item.name} | Radar`;
+		} else {
+			document.title = 'Radar';
+		}
+	}, [item]);
+
 	if (!item) {
 		return null;
 	}
